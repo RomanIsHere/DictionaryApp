d7fda99f71cdf96643265a1b7fdb632f06746a55
Roman Petrica <romanpetrica2895@gmail.com>
2019-10-12 21:50:13 +0300

    3rd commit

--- app1.py
+++ app1.py
@@ -5,8 +5,12 @@ data = json.load(open("data.json"))
 
 
 def translate(word):
-    return data[word]
+    if word in data:
+        return data[word]
+    else:
+        return "The word doesn't exist."
 
-word = input("Enter the word: ")
+word = input("Enter the word: ").lower()
+
 
 print(translate(word))
