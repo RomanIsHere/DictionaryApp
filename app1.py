fff1e5291abe88e466a2aae6e2e4b90caad9ba46
Roman Petrica <romanpetrica2895@gmail.com>
2019-10-12 21:44:07 +0300

    second commit

--- app1.py
+++ app1.py
@@ -1,1 +1,12 @@
 #The first app in the 10ProjectsPython
+import json
+
+data = json.load(open("data.json"))
+
+
+def translate(word):
+    return data[word]
+
+word = input("Enter the word: ")
+
+print(translate(word))
